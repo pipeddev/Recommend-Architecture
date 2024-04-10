10af92536ff9754c2af864d9314763bb83f25203
Felipe Carrasco <carrascogalindo.felipe@gmail.com>
2024-04-10 19:04:14 -0400

    feature: connect fragment with viewmodel

--- app/src/main/java/com/pipe/d/dev/recommendarch/common/di/AdapterModule.kt
+++ app/src/main/java/com/pipe/d/dev/recommendarch/common/di/AdapterModule.kt
@@ -1,6 +1,7 @@
 package com.pipe.d.dev.recommendarch.common.di
 
 import com.pipe.d.dev.recommendarch.common.utils.OnClickListener
+import com.pipe.d.dev.recommendarch.favoriteModule.view.WineFavListAdapter
 import com.pipe.d.dev.recommendarch.homeModule.view.WineDiff
 import com.pipe.d.dev.recommendarch.homeModule.view.WineListAdapter
 import org.koin.dsl.module
@@ -8,4 +9,5 @@ import org.koin.dsl.module
 val adapterModule = module {
     factory { WineDiff()}
     factory<WineListAdapter> { (listener: OnClickListener) -> WineListAdapter(listener, get())  }
+    factory<WineFavListAdapter> { (listener: OnClickListener) -> WineFavListAdapter(listener, get())  }
 }
--- app/src/main/java/com/pipe/d/dev/recommendarch/common/di/DomainModule.kt
+++ app/src/main/java/com/pipe/d/dev/recommendarch/common/di/DomainModule.kt
@@ -1,5 +1,6 @@
 package com.pipe.d.dev.recommendarch.common.di
 
+import com.pipe.d.dev.recommendarch.favoriteModule.model.domain.FavoriteRoomDatabase
 import com.pipe.d.dev.recommendarch.homeModule.model.domain.HomeRoomDatabase
 import com.pipe.d.dev.recommendarch.homeModule.model.domain.HomeWineService
 import org.koin.dsl.module
@@ -7,4 +8,5 @@ import org.koin.dsl.module
 val domainModule = module {
     factory { HomeRoomDatabase(get()) }
     factory { HomeWineService(get()) }
+    factory { FavoriteRoomDatabase(get()) }
 }
