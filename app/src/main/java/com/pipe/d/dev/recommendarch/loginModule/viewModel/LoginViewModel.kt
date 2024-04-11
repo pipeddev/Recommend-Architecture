1516b7397125e0fa2517eba96e366abfbad3381a
Felipe Carrasco <carrascogalindo.felipe@gmail.com>
2024-04-11 12:25:56 -0400

    feature: connect view model with repository and create variables to change values

--- app/src/main/java/com/pipe/d/dev/recommendarch/loginModule/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/pipe/d/dev/recommendarch/loginModule/viewModel/LoginViewModel.kt
@@ -7,15 +7,14 @@ import androidx.lifecycle.viewModelScope
 import com.pipe.d.dev.recommendarch.common.entities.FirebaseUser
 import com.pipe.d.dev.recommendarch.common.entities.MyException
 import com.pipe.d.dev.recommendarch.common.utils.Constants
+import com.pipe.d.dev.recommendarch.common.viewModel.BaseViewModel
 import com.pipe.d.dev.recommendarch.loginModule.model.LoginRepository
 import kotlinx.coroutines.launch
 
-class LoginViewModel(private val repository: LoginRepository): ViewModel() {
-    private val _inProgress = MutableLiveData<Boolean>()
-    val inProgress: LiveData<Boolean> = _inProgress
+class LoginViewModel(private val repository: LoginRepository): BaseViewModel() {
+    val username = MutableLiveData("cursoant")
+    val pin = MutableLiveData("1234")
 
-    private val _snackBarMsg = MutableLiveData<Int>()
-    val snackBarMsg: LiveData<Int> = _snackBarMsg
 
     private val _isValidAuth = MutableLiveData<Boolean>()
     val isValidAuth: LiveData<Boolean> = _isValidAuth
@@ -25,26 +24,20 @@ class LoginViewModel(private val repository: LoginRepository): ViewModel() {
     }
 
     private fun checkAuth() {
-        viewModelScope.launch {
-            _inProgress.value = Constants.SHOW
-            try {
-                _isValidAuth.value = repository.checkAuth()
-            } finally {
-                _inProgress.value = Constants.HIDE
+        executeAction {
+            repository.checkAuth() { result ->
+                _isValidAuth.value = result
             }
         }
     }
 
-    fun login(username: String, pin: String) {
-        viewModelScope.launch {
-            _inProgress.value = Constants.SHOW
-            try {
-                _isValidAuth.value = repository.login(username, pin)
-            } catch (ex: MyException){
-                _snackBarMsg.value = ex.resMsg
-            } finally {
-                _inProgress.value = Constants.HIDE
+    fun login() {
+        executeAction {
+            repository.login(username.value, pin.value) { result ->
+                _isValidAuth.value = result
             }
         }
     }
+
+    override fun onPause() = clearValues()
 }
